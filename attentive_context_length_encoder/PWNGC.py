61c63fb49be659a95ebae26d2bd2702db8835501
MSiba <siba.mohssen@gmail.com>
2022-01-04 00:01:09 +0100

    torchtext and Semcor

---
+++ attentive_context_length_encoder/PWNGC.py
@@ -0,0 +1,6 @@
+"""
+Paper (added 500 new annotations): Completing the Princeton Annotated Gloss Corpus Project
+https://github.com/own-pt/glosstag
+https://github.com/cltl/pwgc
+https://wordnetcode.princeton.edu/glosstag.shtml
+"""
